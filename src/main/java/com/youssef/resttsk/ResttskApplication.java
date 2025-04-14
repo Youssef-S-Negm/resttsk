6dce7a6c9a33fca51991ba99b3ee4c145868c37a
Youssef Negm <youssef.negm2001@gmail.com>
2025-04-14 14:18:57 +0200

    initial commit

---
+++ src/main/java/com/youssef/resttsk/ResttskApplication.java
@@ -0,0 +1,13 @@
+package com.youssef.resttsk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResttskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResttskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youssef/resttsk/ResttskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youssef.resttsk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResttskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
